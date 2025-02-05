e14e057dc507a0f7cd6d005c7392f1eb1dc97259
Aleksandr Morozov <dr.morozov@hotmail.com>
2025-02-04 22:14:27 -0800

    mult, div

--- CPProject/main.cpp
+++ CPProject/main.cpp
@@ -21,7 +21,14 @@ int main () {
     cin >> num_2;
     
     result = num_1 + num_2;
-    cout << "Result is " << result << endl;
+    cout << num_1 << " + " << num_2 << " = " << result << endl;
+    result = num_1 - num_2;
+    cout << num_1 << " - " << num_2 << " = " << result << endl;
+    result = num_1 * num_2;
+    cout << num_1 << " * " << num_2 << " = " << result << endl;
+    result = num_1 / num_2;
+    cout << num_1 << " / " << num_2 << " = " << result << endl;
+    
     
     return 0;
 }
