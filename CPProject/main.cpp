64921efc06b9bad3700760ac2e9daea4ea4b75a3
Aleksandr Morozov <dr.morozov@hotmail.com>
2025-02-04 21:23:13 -0800

    Initial commit

---
+++ CPProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPProject
+//
+//  Created by Aleksandr Morozov on 2/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
