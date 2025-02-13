163f4a13dc1c677b71dd729ace16809cd9b97016
Aleksandr Morozov <dr.morozov@hotmail.com>
2025-02-12 22:02:43 -0800

    day 2

--- CPProject/main.cpp
+++ CPProject/main.cpp
@@ -1,35 +1,33 @@
+////
+////  main.cpp
+////  CPProject
+////
+////  Created by Aleksandr Morozov on 2/4/25.
+////
 //
-//  main.cpp
-//  CPProject
+//#include <iostream>
+//#include <string>
+//#include <vector>
 //
-//  Created by Aleksandr Morozov on 2/4/25.
+//using namespace std;
 //
-
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int main () {
-    int num_1, num_2, result;
-    float res;
-    
-    cout << "Enter num_1: " << endl;
-    cin >> num_1;
-    cout << "Enter num_2: " << endl;
-    cin >> num_2;
-    
-    result = num_1 + num_2;
-    cout << num_1 << " + " << num_2 << " = " << result << endl;
-    result = num_1 - num_2;
-    cout << num_1 << " - " << num_2 << " = " << result << endl;
-    result = num_1 * num_2;
-    cout << num_1 << " * " << num_2 << " = " << result << endl;
-    res = (float)num_1 / (float)num_2;
-    cout << num_1 << " / " << num_2 << " = " << res << endl;
-    
-    
-    
-    return 0;
-}
+//int main () {
+//    int num_1, num_2, result;
+//    float res;
+//    
+//    cout << "Enter num_1: " << endl;
+//    cin >> num_1;
+//    cout << "Enter num_2: " << endl;
+//    cin >> num_2;
+//    
+//    result = num_1 + num_2;
+//    cout << num_1 << " + " << num_2 << " = " << result << endl;
+//    result = num_1 - num_2;
+//    cout << num_1 << " - " << num_2 << " = " << result << endl;
+//    result = num_1 * num_2;
+//    cout << num_1 << " * " << num_2 << " = " << result << endl;
+//    res = (float)num_1 / (float)num_2;
+//    cout << num_1 << " / " << num_2 << " = " << res << endl;
+//    
+//    return 0;
+//}
