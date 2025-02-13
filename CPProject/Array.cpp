76b41aad706bf35c2a6b40c05040b4708e1ef276
Aleksandr Morozov <dr.morozov@hotmail.com>
2025-02-12 23:10:13 -0800

    day 2 finish

---
+++ CPProject/Array.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  CPProject
+//
+//  Created by Aleksandr Morozov on 2/12/25.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    
+    int array[4];
+    
+    return 0;
+}
---
+++ CPProject/Switch.cpp
@@ -0,0 +1,33 @@
+////
+////  main.cpp
+////  CPProject
+////
+////  Created by Aleksandr Morozov on 2/12/25.
+////
+//
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int num = 1 == 1 ? 12 : 23;
+//
+//int main () {
+//    int num = 12;
+//    switch(num) {
+//        case 1:
+//            cout << "Num is 1";
+//            break;
+//        case 2:
+//            cout << "Num is 2";
+//            break;
+//        case 3:
+//            cout << "Num is 3";
+//            break;
+//        default:
+//            cout << "Num is unknown ";
+//            
+//    }
+//    return 0;
+//}
